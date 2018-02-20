ec93a52d0e6d238a6b3b82d76f5d51dc20cf6539
aidanreilly <aidreilly@gmail.com>
2018-02-20 22:47:45 +0000

    simple hash function - works in index.js

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-//https://nodejs.org/en/docs/guides/simple-profiling/
-//to test
-//curl -X GET "http://localhost:8080/newUser?username=matt&password=password"
-//ab -k -c 20 -n 250 "http://localhost:8080/auth?username=matt&password=password"
-//needs curl and ab obvs
+    //https://nodejs.org/en/docs/guides/simple-profiling/
+    //to test
+    //curl -X GET "http://localhost:8080/newUser?username=matt&password=password"
+    //ab -k -c 20 -n 250 "http://localhost:8080/auth?username=matt&password=password"
+    //needs curl and ab obvs
 
-var express = require('express');
-var app = express();
-var crypto = require('crypto');
+    var express = require('express');
+    var app = express();
+    var crypto = require('crypto');
+    //var hash = require('./hashFunction.js');
 
-require('./hashFunction.js')
 
-app.get('/newUser', (req, res) => {
-  let username = req.query.username || '';
-  username = username.replace(/[!@#$%^&*]/g, '');
 
-  if (!username) {
-    return res.sendStatus(400);
-}
+    function createHash(username) {
+      let usr = username;
+      usr = usr.replace(/[!@#$%^&*]/g, '');
+      const salt = crypto.randomBytes(128).toString('base64');
+      const hash = crypto.createHmac('sha256', usr, salt)
+      .update(salt)
+      .digest('hex');
+      username = hash;
+      return username;
+};
 
-const salt = crypto.randomBytes(128).toString('base64');
-const hash = crypto.createHmac('sha256', username, salt)
-.update(salt)
-.digest('hex');
+    app.get('/newUser', (req, res) => {
+        var hashed = createHash(req.query.username);
+        console.log('hashed username = '+hashed);
+        res.sendStatus(200);
+    });
 
-username = { salt, hash };
-
-console.log('hashed username = '+hash);
-res.sendStatus(200);
-});
-
-app.listen(8080, function () {
-  console.log('listening on port 8080!');
-});
+    app.listen(8080, function () {
+      console.log('listening on port 8080!');
+  });
